e5ef86e05ceb59ac22deaa31e198d1ff173facf1
lgerberon <leo.gerberon@lacooperativewelcoop.com>
2021-10-25 15:44:40 +0200

    Corrige context

--- app/src/main/java/com/pharmagestmonalisa/dojo/dojoms/DojoMsApplication.java
+++ app/src/main/java/com/pharmagestmonalisa/dojo/dojoms/DojoMsApplication.java
@@ -3,7 +3,7 @@ package com.pharmagestmonalisa.dojo.dojoms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.pharmagest.monalisa.dojo.core")
 public class DojoMsApplication {
 
 	public static void main(String[] args) {
